6d9f201eeaf87f045d3e45481790cd7af421e638
sehwa <love7620640@naver.com>
2023-08-22 21:47:33 +0900

    refact: 세화 설명주석 추가

--- August_/230823/176963_memory_score/176963_sehwa.js
+++ August_/230823/176963_memory_score/176963_sehwa.js
@@ -2,7 +2,6 @@ function solution(name, yearning, photo) {
   let answer = [];
   let score = 0;
 
-  // name[i] 이면, yearning[i]
   for (let j = 0; j < photo.length; j++) {
     for (let k = 0; k < photo[j].length; k++) {
       for (let i = 0; i < name.length; i++) {
--- August_/230823/178871_run_race/178871_sehwa.js
+++ August_/230823/178871_run_race/178871_sehwa.js
@@ -1,11 +1,19 @@
 function solution(players, callings) {
   for (let i = 0; i < callings.length; i++) {
-    for (let j = 0; j < players.length; j++) {
+    for (let j = 1; j < players.length; j++) {
       if (callings[i] === players[j]) {
         [players[j - 1], players[j]] = [players[j], players[j - 1]];
       } else continue;
     }
   }
+
+  // callings[0] === players[1] 일치하면
+  // 앞에 원소와 순서 변경
+
+  // [a, b] = [b, a]
+
+  // let a = players[0], b = players[1]
+  // [players[0], players[1]] = [players[1], players[0]]
 
   return players;
 }
