855bebb87b8b6ab924c89856aed01d8d7d716301
lemon4974 <skyblue8323@gmail.com>
2023-09-26 22:37:54 +0900

    feat: 제윤 풀이

---
+++ September_/230927/120883/120883_jeyoon.js
@@ -0,0 +1,17 @@
+function solution(id_pw, db) {
+    let answer = '';
+    for(let i = 0 ; i < db.length ; i++){
+        if( id_pw[0] == db[i][0]){ //아이디 일치
+            if(id_pw[1] == db[i][1]){ //비밀번호 일치
+             answer = 'login';     
+            }else if(id_pw[1] !== db[i][1]){
+             answer = 'wrong pw';
+            }
+        } else if(id_pw[0] !== db[i][0] && id_pw[1] === db[i][1]){
+              answer = 'fail';
+        }
+    }
+    return answer;
+}
+
+//test case 1에서 계속 실패하는 이유가 알고싶네요...
